12ef24dba81f93652b5619f9d2b54fcbb73a179b
mike cohen <mcohen113@msn.com>
2017-02-06 18:39:39 -0500

    modal pops up from contact button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,6 @@
-$(document).ready(function(){ $(".button-collapse").sideNav(); });
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.modal').modal();
+});
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+
