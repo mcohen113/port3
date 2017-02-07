5c4df3c4fc82dd6a317b3c256f1c07bc7b8d3fa9
mike cohen <mcohen113@msn.com>
2017-02-06 19:17:34 -0500

    fields of modal appear

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
-  $('.modal').modal();
+  $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        console.log("modal open");
+        console.log(modal, trigger);
+      },
+      complete: function() { console.log("modal closed"); } // Callback for Modal close
+    }
+  );;
 });
    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 
