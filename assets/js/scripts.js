ddc57f664da5de5f7ba7cf3904b3c69af014293d
mike cohen <mcohen113@msn.com>
2017-02-15 16:33:45 -0500

    speed of modal popup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $('.modal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .5, // Opacity of modal background
-      inDuration: 300, // Transition in duration
+      inDuration: 350, // Transition in duration
       outDuration: 200, // Transition out duration
       startingTop: '4%', // Starting top style attribute
       endingTop: '10%', // Ending top style attribute
